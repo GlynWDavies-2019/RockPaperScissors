ac19226a2e97c8c293456df549e62cd54ffc750c
Glyn Davies <glynwdavies@gmail.com>
2020-06-06 13:52:15 +0100

    Styling completed

---
+++ script.js
@@ -0,0 +1,4 @@
+const choices = document.querySelectorAll('.choice');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
